72c84427815387c0fa03cec35ab17c1f15735234
joannacollier <collj035@gmail.com>
2020-06-01 17:06:09 +0100

    Initial folder structure set up and testing framework running, Updated readme

---
+++ ten_walk_JS/jasmine-standalone-3.5.0/spec/tenMinWalkSpec.js
@@ -0,0 +1,9 @@
+// describe( "Walk", function(){
+//   var walk 
+
+//   beforeEach(function(){
+//     walk = new Walk; 
+//   })
+
+//   it ('.direction is returning a string of n, s, e w')
+// })
