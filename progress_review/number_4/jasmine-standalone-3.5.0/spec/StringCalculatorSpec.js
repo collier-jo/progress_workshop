d243ba3ec9ef3d3aec9b34acc59dcc040342bd5a
joannacollier <collj035@gmail.com>
2020-08-06 11:23:30 +0100

    multiple additions

--- progress_review/number_4/jasmine-standalone-3.5.0/spec/StringCalculatorSpec.js
+++ progress_review/number_4/jasmine-standalone-3.5.0/spec/StringCalculatorSpec.js
@@ -2,13 +2,21 @@
 
 describe("StringCalculator", function(){
 
+    var stringCalc
+
+    beforeEach(function(){
+      stringCalc = new StringCalculator
+    });
+
     it("Simple sum 1 + 1", function(){
-      var stringCalc = new StringCalculator 
       expect(stringCalc.calculate("1 + 1")).toEqual(["1 + 1", 2])
     });
 
     it("Simple sum 2 + 2", function(){
-      var stringCalc = new StringCalculator
       expect(stringCalc.calculate("2 + 2")).toEqual(["2 + 2", 4])
+    });
+
+    it("More than two numbers in sum", function(){
+      expect(stringCalc.calculate("2 + 2 + 2")).toEqual(["2 + 2 + 2", 6])
     });
 })
--- progress_review/number_4/jasmine-standalone-3.5.0/src/StringCalculator.js
+++ progress_review/number_4/jasmine-standalone-3.5.0/src/StringCalculator.js
@@ -2,19 +2,17 @@ class StringCalculator {
 
     calculate(string){
 
-        var equationArray = string.split(" ")
-        // ["2" "+" "2"]
-        // 1st and last in array = numbers 
+        var sumArray = string.split(" ")
+        var sum = 0
 
-        var firstNumber = Number(equationArray[0]) // 2 
-        var secondNumber = Number(equationArray[2]) // 2
-
-        if(equationArray[1] === "+"){
-            let equals = firstNumber + secondNumber
-            return [string, equals]
+        for(var i = 0; i <= sumArray.length - 1; i ++){
+            var singleItem = sumArray[i]
+            if(!(singleItem === "+")){
+                var convertedNumber = Number(singleItem)
+                sum += convertedNumber
+            } 
         }
 
-
-        // return [string, 2]
+        return [string, sum]
     }
 }
