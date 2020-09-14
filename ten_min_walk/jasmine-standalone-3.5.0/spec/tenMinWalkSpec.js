907dbff8145f3ddb6113807fc1d5300d5b275e01
joannacollier <collj035@gmail.com>
2020-09-14 14:12:55 +0100

    Accumulor returning true and false

--- ten_min_walk/jasmine-standalone-3.5.0/spec/tenMinWalkSpec.js
+++ ten_min_walk/jasmine-standalone-3.5.0/spec/tenMinWalkSpec.js
@@ -1,11 +1,18 @@
 'use strict';
 
 describe("Walking Generator", () => {
-
-
-
     it("Takes an array checks the length is 10 to return true", () => {
         var walkingGen = new WalkingGenerator 
         expect(walkingGen.tenMinWalk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).toEqual(true)
+    })
+
+    it("Returns false if array less than 10", () => {
+        var walkingGen = new WalkingGenerator 
+        expect(walkingGen.tenMinWalk(['w', 's'])).toEqual(false)
+    })
+
+    it("Returns false to length 10 but not to start point", () => {
+        var walkingGen = new WalkingGenerator 
+        expect(walkingGen.tenMinWalk(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).toEqual(false)
     })
 })
--- ten_min_walk/jasmine-standalone-3.5.0/src/tenMinWalk.js
+++ ten_min_walk/jasmine-standalone-3.5.0/src/tenMinWalk.js
@@ -1,13 +1,35 @@
 class WalkingGenerator {
 
     tenMinWalk(array){
-       return this._isTenMins(array)
-    }
-    
-    _isTenMins(array){
         if(array.length === 10){
-            return true
+
+            //iterate over the array 
+
+            var x = 0
+            var y = 0 
+
+            array.forEach(direction => {
+                if(direction === "n"){
+                    y += 1
+                } else if(direction === "s"){
+                    y -= 1
+                } else if(direction === "e"){
+                    x -=1
+                } else {
+                    x += 1
+                }
+            })
+
+            if( x === 0 && y === 0){
+                return true
+            } else {
+                return false
+            }
+            
+        } else {
+            return false
         }
     }
-
 }
+
+// array1.forEach(element => console.log(element));
