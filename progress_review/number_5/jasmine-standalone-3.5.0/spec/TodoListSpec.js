f6b7c7737fae873de37f10e7cc9f9a11a7b39162
joannacollier <collj035@gmail.com>
2020-09-16 13:46:47 +0100

    Todo list add un hardcoded

--- progress_review/number_5/jasmine-standalone-3.5.0/spec/TodoListSpec.js
+++ progress_review/number_5/jasmine-standalone-3.5.0/spec/TodoListSpec.js
@@ -4,6 +4,13 @@ describe("To Do list", () => {
 
   it("Can add a todo to list", () => {
     var todo = new TodoList
-    expect(todo.add("Get soya milk")).toEqual("1. Get soya milk")
+    expect(todo.add("Get soya milk")).toEqual("1. Get soya milk\n")
   })
+
+  it("Add two items to list", () => {
+    var todo = new TodoList 
+    todo.add("Get hair cut")
+    expect(todo.add("Doctors app")).toEqual("1. Get hair cut\n2. Doctors app\n")
+  })
+
 })
--- progress_review/number_5/jasmine-standalone-3.5.0/src/TodoList.js
+++ progress_review/number_5/jasmine-standalone-3.5.0/src/TodoList.js
@@ -1,9 +1,22 @@
 class TodoList {
   constructor(){
     this.list = []
+    this.index = 0
   }
 
+
   add(text){
-    return `1. ${text}`
+    this.index ++
+    this.list.push(`${this.index}. ${text}`)
+
+    var viewList = ""
+
+    this.list.forEach(element => 
+      viewList += element + "\n"
+    )
+
+    return viewList
+    
+    
   }
 }
