9bd7e913e01f39fde06cd92d096456c424b6c055
joannacollier <collj035@gmail.com>
2020-09-16 14:48:41 +0100

    Refactored add into seperte method

--- progress_review/number_5/jasmine-standalone-3.5.0/src/TodoList.js
+++ progress_review/number_5/jasmine-standalone-3.5.0/src/TodoList.js
@@ -5,38 +5,32 @@ class TodoList {
   todo(text){
     var userInputArray = text.split(" ")
 
-    if(userInputArray[0] === "add"){
-      userInputArray.splice(0,1)
-      var todo = userInputArray.join(" ")
-      
-      this.list.push(todo)
-
-      var format = ""
-
-      this.list.forEach((element, index) => {
-        format += `${index + 1}. ${element}\n` 
-      })
-
-      return format
-
+  
+    if(userInputArray[0] === "add") {
+      this._add(userInputArray)
     } else if(userInputArray[0] === "done"){
       var itemNumber = userInputArray[1] - 1
       this.list.splice(itemNumber, 1)
-
-      var format = ""
-
-      this.list.forEach((element, index) => {
-        format += `${index + 1}. ${element}\n` 
-      })
-
-      return format
-
-
-
     } else {
       return "Invalid Input: You should use add or done with a space after"
     }
 
+    var format = ""
+
+    this.list.forEach((element, index) => {
+      format += `${index + 1}. ${element}\n` 
+    })
+
+    return format
+
+  }
+
+  _add(array){
+    console.log("inside add")
+    array.splice(0,1)
+    var todo = array.join(" ")
+    this.list.push(todo)
   }
 }
 
+//var variable = (condition) ? (true block) : ((condition2) ? (true block2) : (else block2))
